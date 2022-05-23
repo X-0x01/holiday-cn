d800860fa0917b7ecf5c07b12c3f1af5f75b36ac
X-0x01 <19320096+X-0x01@users.noreply.github.com>
2022-05-23 11:51:40 +0800

    Update fetch_holidays.py

--- fetch_holidays.py
+++ fetch_holidays.py
@@ -42,7 +42,29 @@ PRE_PARSED_PAPERS = {
             "date": date(2015, 9, 6),
             "isOffDay": False,
         },
-    ]
+    ],
+    "http://www.gov.cn/zhengce/content/2020-01/27/content_5472352.htm": [
+        {
+            "name": "春节",
+            "date": date(2020, 1, 31),
+            "isOffDay": True,
+        },
+        {
+            "name": "春节",
+            "date": date(2020, 2, 1),
+            "isOffDay": True,
+        },
+        {
+            "name": "春节",
+            "date": date(2020, 2, 2),
+            "isOffDay": True,
+        },
+        {
+            "name": "春节",
+            "date": date(2020, 2, 3),
+            "isOffDay": False,
+        },
+    ],
 }
 
 
@@ -236,14 +258,6 @@ class DescriptionParser:
 class SentenceParser:
     """Parser for holiday shift description sentence."""
 
-    special_cases = {
-        "延长2020年春节假期至2月2日（农历正月初九": [
-            {"date": date(2020, 1, 31), "isOffDay": True},
-            {"date": date(2020, 2, 1), "isOffDay": True},
-            {"date": date(2020, 2, 2), "isOffDay": True},
-        ],
-    }
-
     def __init__(self, parent: DescriptionParser, sentence):
         self.parent = parent
         self.sentence = sentence
@@ -281,6 +295,7 @@ class SentenceParser:
             yield self.parent.get_date(year=groups[0], month=groups[1], day=groups[2])
 
     def _extract_dates_2(self, value: str) -> Iterator[date]:
+        value = re.sub(r"（.+?）", "", value)
         match = re.findall(
             r"(?:(\d+)年)?(?:(\d+)月)?(\d+)日(?:至|-|—)(?:(\d+)年)?(?:(\d+)月)?(\d+)日", value
         )
@@ -293,6 +308,7 @@ class SentenceParser:
                 yield start + timedelta(days=i)
 
     def _extract_dates_3(self, value: str) -> Iterator[date]:
+        value = re.sub(r"（.+?）", "", value)
         match = re.findall(
             r"(?:(\d+)年)?(?:(\d+)月)?(\d+)日(?:（[^）]+）)?"
             r"(?:、(?:(\d+)年)?(?:(\d+)月)?(\d+)日(?:（[^）]+）)?)+",
@@ -341,15 +357,10 @@ class SentenceParser:
             for i in self.extract_dates(match.group(2)):
                 yield {"date": i, "isOffDay": True}
 
-    def _parse_special(self):
-        for i in self.special_cases.get(self.sentence, []):
-            yield i
-
     parsing_methods = [
         _parse_rest_1,
         _parse_work_1,
         _parse_shift_1,
-        _parse_special,
     ]
 
 
